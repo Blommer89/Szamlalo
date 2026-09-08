8b9a08feb9244610669fb674abd838ad882af76f
Blommer89 <attika2519@gmail.com>
2026-09-08 12:47:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,94 +1,168 @@
-const CACHE_NAME = 'szamlalo-cache-v1';
+const CACHE_NAME = 'szamlalo-cache-v2';
+
 
 const FILES_TO_CACHE = [
+
     './',
+
     './index.html',
+
     './manifest.json',
+
     './ikon.png',
+
     './ikon_kacsinto.png'
+
 ];
+
 
 /*
  * Telepítés
  */
-self.addEventListener('install', event => {
 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
-    );
+self.addEventListener(
+    'install',
+    event => {
 
-});
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            )
+
+            .then(
+                cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+            )
+
+            .then(
+                () =>
+                    self.skipWaiting()
+            )
+
+        );
+
+    }
+);
 
 
 /*
  * Aktiválás
  */
-self.addEventListener('activate', event => {
 
-    event.waitUntil(
+self.addEventListener(
+    'activate',
+    event => {
 
-        caches.keys()
-            .then(keys => {
+        event.waitUntil(
 
-                return Promise.all(
+            caches.keys()
 
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
+                .then(
+                    keys => {
 
-                );
+                        return Promise.all(
 
-            })
-            .then(() => self.clients.claim())
+                            keys
 
-    );
+                                .filter(
+                                    key =>
+                                        key !==
+                                        CACHE_NAME
+                                )
 
-});
+                                .map(
+                                    key =>
+                                        caches.delete(
+                                            key
+                                        )
+                                )
+
+                        );
+
+                    }
+                )
+
+                .then(
+                    () =>
+                        self.clients.claim()
+                )
+
+        );
+
+    }
+);
 
 
 /*
  * Hálózati kérés kezelése
  *
- * Először megpróbáljuk az aktuális fájlt letölteni,
- * hiba esetén pedig a gyorsítótárból adjuk vissza.
+ * Elsőként az aktuális fájlt próbáljuk
+ * letölteni.
+ *
+ * Ha nincs internet vagy nem sikerül
+ * a letöltés, akkor a gyorsítótárból
+ * adjuk vissza.
  */
-self.addEventListener('fetch', event => {
 
-    if (event.request.method !== 'GET') {
-        return;
+self.addEventListener(
+    'fetch',
+    event => {
+
+        if (
+            event.request.method !==
+            'GET'
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            )
+
+            .then(
+                response => {
+
+                    const copy =
+                        response.clone();
+
+
+                    caches.open(
+                        CACHE_NAME
+                    )
+
+                    .then(
+                        cache => {
+
+                            cache.put(
+                                event.request,
+                                copy
+                            );
+
+                        }
+                    );
+
+
+                    return response;
+
+                }
+            )
+
+            .catch(
+                () =>
+                    caches.match(
+                        event.request
+                    )
+            )
+
+        );
+
     }
-
-    event.respondWith(
-
-        fetch(event.request)
-            .then(response => {
-
-                /*
-                 * Friss válasz elmentése.
-                 */
-                const copy = response.clone();
-
-                caches.open(CACHE_NAME)
-                    .then(cache => {
-                        cache.put(
-                            event.request,
-                            copy
-                        );
-                    });
-
-                return response;
-
-            })
-            .catch(() => {
-
-                return caches.match(
-                    event.request
-                );
-
-            })
-
-    );
-
-});
+);
